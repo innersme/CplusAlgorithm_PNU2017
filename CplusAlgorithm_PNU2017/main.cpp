d4e59e781e0872a540b9502bd97852eae8bf286f
innersme <innersme@gmail.com>
2017-10-21 01:05:19 +0900

    Initial Commit

---
+++ CplusAlgorithm_PNU2017/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CplusAlgorithm_PNU2017
+//
+//  Created by MJMac on 2017. 10. 21..
+//  Copyright © 2017년 innersme. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
